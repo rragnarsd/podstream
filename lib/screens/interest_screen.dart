caa530c13c7bb3437a35f2f2eff104328bab3a35
Regina Ragnarsdottir <reginaragnarsd@gmail.com>
2025-04-29 09:55:50 +0000

    Border changes

--- lib/screens/interest_screen.dart
+++ lib/screens/interest_screen.dart
@@ -1,62 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:podstream/screens/subscribe_screen.dart';
 
-class InterestScreen extends StatelessWidget {
+class InterestScreen extends StatefulWidget {
   const InterestScreen({super.key});
+
+  @override
+  State<InterestScreen> createState() => _InterestScreenState();
+}
+
+class _InterestScreenState extends State<InterestScreen> {
+  List<InterestData> selectedInterests = [];
+
+  void toggleInterest(InterestData interest) {
+    setState(() {
+      if (selectedInterests.contains(interest)) {
+        selectedInterests.remove(interest);
+      } else {
+        selectedInterests.add(interest);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Choose your interests'),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Text('Skip', style: TextStyle(color: Colors.grey.shade400)),
-          ),
-        ],
-      ),
+      appBar: AppBar(title: const Text('Choose your interests')),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
             const Text(
               'Choose your interest and get the best podcast for you. You can choose multiple interests.',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
             ),
             const SizedBox(height: 24),
             Wrap(
-              spacing: 12,
-              runSpacing: 4,
+              spacing: 16,
+              runSpacing: 6,
               children:
                   interests
-                      .map((interest) => Chip(label: Text(interest)))
+                      .map(
+                        (interest) => GestureDetector(
+                          onTap: () => toggleInterest(interest),
+                          child: Chip(
+                            label: Text(
+                              interest.interest,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                color:
+                                    selectedInterests.contains(interest)
+                                        ? interest.textColor
+                                        : Colors.black,
+                              ),
+                            ),
+                            side: const BorderSide(width: 3),
+                            backgroundColor:
+                                selectedInterests.contains(interest)
+                                    ? interest.color
+                                    : Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ),
+                      )
                       .toList(),
             ),
             const Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.redAccent,
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SubscribeScreen(),
-                    ),
-                  );
-                },
-                child: const Text(
-                  'Continue',
-                  style: TextStyle(fontSize: 16, color: Colors.white),
-                ),
-              ),
+            const OnboardingButtonBar(
+              continueText: 'Continue',
+              continueWidget: SubscribeScreen(),
             ),
             const SizedBox(height: 24),
           ],
@@ -66,28 +80,208 @@ class InterestScreen extends StatelessWidget {
   }
 }
 
-List<String> interests = [
-  'Technology',
-  'Science',
-  'Business',
-  'Health',
-  'Education',
-  'Politics',
-  'History',
-  'Art',
-  'Music',
-  'Sports',
-  'Travel',
-  'Food',
-  'Fashion',
-  'Lifestyle',
-  'Entertainment',
-  'News',
-  'Gaming',
-  'True Crime',
-  'Comedy',
-  'Technology',
-  'Science',
-  'Business',
-  'Health',
+List<InterestData> interests = [
+  InterestData(
+    interest: 'Technology',
+    isSelected: false,
+    color: Colors.blue,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Science',
+    isSelected: false,
+    color: Colors.green,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Business',
+    isSelected: false,
+    color: Colors.purple,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Health',
+    isSelected: false,
+    color: Colors.red,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Education',
+    isSelected: false,
+    color: Colors.orange,
+    textColor: Colors.black,
+  ),
+  InterestData(
+    interest: 'Politics',
+    isSelected: false,
+    color: Colors.indigo,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'History',
+    isSelected: false,
+    color: Colors.brown,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Art',
+    isSelected: false,
+    color: Colors.pink,
+    textColor: Colors.black,
+  ),
+  InterestData(
+    interest: 'Music',
+    isSelected: false,
+    color: Colors.teal,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Sports',
+    isSelected: false,
+    color: Colors.amber,
+    textColor: Colors.black,
+  ),
+  InterestData(
+    interest: 'Travel',
+    isSelected: false,
+    color: Colors.cyan,
+    textColor: Colors.black,
+  ),
+  InterestData(
+    interest: 'Food',
+    isSelected: false,
+    color: Colors.deepOrange,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Fashion',
+    isSelected: false,
+    color: Colors.deepPurple,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Lifestyle',
+    isSelected: false,
+    color: Colors.lightBlue,
+    textColor: Colors.black,
+  ),
+  InterestData(
+    interest: 'Entertainment',
+    isSelected: false,
+    color: Colors.lightGreen,
+    textColor: Colors.black,
+  ),
+  InterestData(
+    interest: 'News',
+    isSelected: false,
+    color: Colors.lime,
+    textColor: Colors.black,
+  ),
+  InterestData(
+    interest: 'Gaming',
+    isSelected: false,
+    color: Colors.yellow,
+    textColor: Colors.black,
+  ),
+  InterestData(
+    interest: 'True Crime',
+    isSelected: false,
+    color: Colors.redAccent,
+    textColor: Colors.white,
+  ),
+  InterestData(
+    interest: 'Comedy',
+    isSelected: false,
+    color: Colors.amberAccent,
+    textColor: Colors.black,
+  ),
 ];
+
+class InterestData {
+  final String interest;
+  final bool isSelected;
+  final Color color;
+  final Color textColor;
+
+  InterestData({
+    required this.interest,
+    required this.isSelected,
+    required this.color,
+    required this.textColor,
+  });
+}
+
+class OnboardingButtonBar extends StatelessWidget {
+  const OnboardingButtonBar({
+    super.key,
+
+    required this.continueText,
+    required this.continueWidget,
+  });
+
+  final String continueText;
+  final Widget continueWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(width: 3),
+              ),
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  'Skip',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 12),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(width: 3),
+              ),
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => continueWidget),
+                  );
+                },
+                child: const Text(
+                  'Continue',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/subscribe_screen.dart
+++ lib/screens/subscribe_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:podstream/screens/interest_screen.dart';
 
 class SubscribeScreen extends StatelessWidget {
   const SubscribeScreen({super.key});
@@ -6,84 +7,173 @@ class SubscribeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Subscribe Authors'),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Text('Skip', style: TextStyle(color: Colors.grey.shade400)),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
+      appBar: AppBar(title: const Text('Subscribe Authors')),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          children: [
-            const Text(
+          children: <Widget>[
+            Text(
               'Subscribe to your favorite authors and get the latest podcasts.',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
             ),
-            const SizedBox(height: 12),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.asset(
-                                'assets/avatar1.jpg',
-                                height: 100,
-                                width: 100,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            const SizedBox(width: 12),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Author $index',
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                                Text(
-                                  'Podcast $index',
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        IconButton(
-                          style: IconButton.styleFrom(
-                            shape: CircleBorder(
-                              side: BorderSide(color: Colors.grey.shade400),
-                            ),
-                          ),
-                          onPressed: () {},
-                          icon: const Icon(Icons.add),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+            SizedBox(height: 12),
+            SubScribeList(),
+            OnboardingButtonBar(
+              continueText: 'Continue',
+              //TODO add continueWidget
+              continueWidget: Text('hi'),
             ),
+            SizedBox(height: 24),
           ],
         ),
       ),
     );
   }
 }
+
+class SubScribeList extends StatefulWidget {
+  const SubScribeList({super.key});
+
+  @override
+  State<SubScribeList> createState() => _SubScribeListState();
+}
+
+class _SubScribeListState extends State<SubScribeList> {
+  List<bool> isSelectedList = List<bool>.generate(
+    subscribeAuthorData.length,
+    (index) => false,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: subscribeAuthorData.length,
+        itemBuilder: (context, index) {
+          final authorData = subscribeAuthorData[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(width: 3),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4),
+                        child: Image.asset(
+                          authorData.image,
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 12),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          authorData.podCastName,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          '${authorData.podCastQty} Podcasts',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                IconButton(
+                  style: IconButton.styleFrom(
+                    backgroundColor:
+                        isSelectedList[index] ? Colors.redAccent : Colors.white,
+                    shape: CircleBorder(
+                      side: BorderSide(width: isSelectedList[index] ? 0 : 3),
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isSelectedList[index] = !isSelectedList[index];
+                    });
+                  },
+                  icon: Icon(
+                    isSelectedList[index] ? Icons.check : Icons.add,
+                    color: isSelectedList[index] ? Colors.white : null,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SubscribeAuthorData {
+  final String podCastName;
+  final String podCastQty;
+  final String image;
+
+  SubscribeAuthorData({
+    required this.podCastName,
+    required this.podCastQty,
+    required this.image,
+  });
+}
+
+List<SubscribeAuthorData> subscribeAuthorData = [
+  SubscribeAuthorData(
+    podCastName: 'Podcast 1',
+    podCastQty: '10',
+    image: 'assets/avatar1.jpg',
+  ),
+  SubscribeAuthorData(
+    podCastName: 'Podcast 2',
+    podCastQty: '100',
+    image: 'assets/avatar2.jpg',
+  ),
+  SubscribeAuthorData(
+    podCastName: 'Podcast 3',
+    podCastQty: '160',
+    image: 'assets/avatar3.jpg',
+  ),
+  SubscribeAuthorData(
+    podCastName: 'Podcast 4',
+    podCastQty: '1000',
+    image: 'assets/avatar4.jpg',
+  ),
+  SubscribeAuthorData(
+    podCastName: 'Podcast 5',
+    podCastQty: '10000',
+    image: 'assets/avatar5.jpg',
+  ),
+  SubscribeAuthorData(
+    podCastName: 'Podcast 6',
+    podCastQty: '90',
+    image: 'assets/avatar6.jpg',
+  ),
+  SubscribeAuthorData(
+    podCastName: 'Podcast 7',
+    podCastQty: '100',
+    image: 'assets/avatar7.jpg',
+  ),
+  SubscribeAuthorData(
+    podCastName: 'Podcast 2',
+    podCastQty: '100',
+    image: 'assets/avatar2.jpg',
+  ),
+];
